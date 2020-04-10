8ad29571c6c6597dc11988042eedc4425998bddc
Juan Alvarez <nebiros@gmail.com>
2020-04-10 13:39:07 -0500

    Add trie data structure

---
+++ trie/trie.go
@@ -0,0 +1,54 @@
+package trie
+
+const (
+	alphabetSize = 26
+)
+
+type trieNode struct {
+	children  [alphabetSize]*trieNode
+	isWordEnd bool
+}
+
+// Trie, tree data structure: https://en.wikipedia.org/wiki/Trie.
+// Based on https://golangbyexample.com/trie-implementation-in-go/
+type trie struct {
+	root *trieNode
+}
+
+func (t *trie) insert(word string) {
+	wordLength := len(word)
+	current := t.root
+
+	for i := 0; i < wordLength; i++ {
+		index := word[i] - 'a'
+
+		if current.children[index] == nil {
+			current.children[index] = &trieNode{}
+		}
+
+		current = current.children[index]
+	}
+
+	current.isWordEnd = true
+}
+
+func (t *trie) find(word string) bool {
+	wordLength := len(word)
+	current := t.root
+
+	for i := 0; i < wordLength; i++ {
+		index := word[i] - 'a'
+
+		if current.children[index] == nil {
+			return false
+		}
+
+		current = current.children[index]
+	}
+
+	return current.isWordEnd
+}
+
+func main() {
+
+}
---
+++ trie/trie_test.go
@@ -0,0 +1,44 @@
+package trie
+
+import (
+	"testing"
+)
+
+func TestTrie(t *testing.T) {
+	tr := &trie{root: &trieNode{}}
+
+	seed := []string{"sam", "john", "tim", "jose", "rose",
+		"cat", "dog", "dogg", "roses"}
+	for i := 0; i < len(seed); i++ {
+		tr.insert(seed[i])
+	}
+	
+	type test struct {
+		w string
+		out bool
+	}
+
+	testcases := []test{
+		{w: "sam", out: true},
+		{w: "john", out: true},
+		{w: "tim", out: true},
+		{w: "jose", out: true},
+		{w: "rose", out: true},
+		{w: "cat", out: true},
+		{w: "dog", out: true},
+		{w: "dogg", out: true},
+		{w: "roses", out: true},
+		{w: "rosess", out: false},
+		{w: "ans", out: false},
+		{w: "san", out: false},
+	}
+
+	t.Parallel()
+
+	for _, c := range testcases {
+		out := tr.find(c.w)
+		if out != c.out {
+			t.Errorf("'%s' not found", c.w)
+		}
+	}
+}
